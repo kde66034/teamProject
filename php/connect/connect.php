500fce0f5187e752d0004e661ba4968473bf20e0
kde66034 <kde66034@gmail.com>
2022-10-11 23:32:16 +0900

    221011

---
+++ php/connect/connect.php
@@ -0,0 +1,23 @@
+<?php
+    $host = "localhost";
+    $user = "kde9902";
+    $pass = "zheldrhdcor02!8";
+    $db = "kde9902";
+    $connect = new mysqli($host, $user, $pass, $db);
+    $connect -> set_charset("utf8");
+
+    // if(mysqli_connect_errno()){
+    //     echo "Database Connect False";
+    // } else {
+    //     echo "Database Connect True";
+    // }
+
+    // http://earth52.dothome.co.kr/
+
+    // $host = "localhost";
+    // $user = "earth52";
+    // $pass = "love25r!";
+    // $db = "earth52";
+    // $connect = new mysqli($host, $user, $pass, $db);
+    // $connect -> set_charset("utf8");
+?>
